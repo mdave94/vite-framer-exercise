3554961a4bdfac51df89ee4b1f705d0e75f62341
molnar.david <molnar.david@wigner.hu>
2024-05-07 13:39:27 +0200

    hero section animation update

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,19 +1,54 @@
+import { stagger } from "framer-motion";
 import "./hero.scss";
+import { motion } from "framer-motion";
+
+const textVariants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
 
 const Hero = () => {
   return (
     <div className="hero">
-      <div className="wrapper">
-        <div className="textContainer">
-          <h2> LOREM IPSUM </h2>
-          <h1>Web Development</h1>
-          <div className="buttons">
-            <button>Get Started</button>
-            <button>Learn More</button>
-          </div>
-          <img src="/scroll.png" alt="" />
-        </div>
-      </div>
+      <motion.div className="wrapper">
+        <motion.div
+          className="textContainer"
+          variants={textVariants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVariants}> LOREM IPSUM </motion.h2>
+          <motion.h1 variants={textVariants}>Web Development</motion.h1>
+          <motion.div className="buttons" variants={textVariants}>
+            <motion.button variants={textVariants}>Get Started</motion.button>
+            <motion.button variants={textVariants}>Learn More</motion.button>
+          </motion.div>
+          <motion.img
+            variants={textVariants}
+            animate="scrollButton"
+            src="/scroll.png"
+            alt=""
+          />
+        </motion.div>
+      </motion.div>
 
       <div className="slidingTextContainer">Developer Programmer</div>
 
