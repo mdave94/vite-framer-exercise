a6cf1f91dbbb48a7c195c1403ba6be20e76428e1
molnar.david <molnar.david@wigner.hu>
2024-05-28 17:24:05 +0200

    parallax setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./app.scss";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Parallax from "./components/parallax/Parallax";
 import Sidebar from "./components/sidebar/Sidebar";
 const App = () => {
   return (
@@ -9,9 +10,13 @@ const App = () => {
         <Navbar />
         <Hero />
       </section>
-      <section id="Services">Parallax</section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
       <section>Services</section>
-      <section id="Portfolio">Parallax</section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
       <section>Portfolio1</section>
       <section>portfolio2</section>
       <section>Portfolio3</section>
