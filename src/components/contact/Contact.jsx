5139a68d29f85b473cbfb0562828a3351df76b21
molnar.david <molnar.david@wigner.hu>
2024-08-09 16:47:24 +0200

    contact form design update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,33 @@
 import "./contact.scss";
 
 const Contact = () => {
-  return <div className="contact">CONTACT</div>;
+  return (
+    <div className="contact">
+      <div className="textContainer">
+        <h1>Let's work together</h1>
+        <div className="item">
+          <h2>Mail</h2>
+          <span>david@molnardavid.hu</span>
+        </div>
+        <div className="item">
+          <h2>Address</h2>
+          <span>Hello there</span>
+        </div>
+        <div className="item">
+          <h2>Phone</h2>
+          <span>+361234567</span>
+        </div>
+      </div>
+      <div className="formContainer">
+        <form action="">
+          <input type="text" required placeholder="Name" />
+          <input type="email" required placeholder="Email" />
+          <textarea rows={8} placeholder="Message" />
+          <button>Contact Me</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
